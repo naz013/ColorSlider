876801c4e29da74d7b802c390505f3614367ca42
Nazar Suhovich <n.suhovich@gmail.com>
2019-02-10 11:25:13 +0200

    Update demo.

--- app/src/main/java/com/colorslider/example/MainActivity.java
+++ app/src/main/java/com/colorslider/example/MainActivity.java
@@ -1,11 +1,15 @@
 package com.colorslider.example;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.github.naz013.colorslider.ColorSlider;
 
@@ -52,5 +56,26 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(color);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_more) {
+            showGithub();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showGithub() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://github.com/naz013/ColorSlider"));
+        startActivity(intent);
     }
 }
